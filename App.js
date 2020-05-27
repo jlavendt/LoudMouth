6efd2ba73aa3846a9f8d1889df81fa09423ef456
Jesse <jlavendt@masonlive.gmu.edu>
2020-05-27 01:08:42 -0400

    added a slider:

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Slider } from 'react-native';
 
 export default function App() {
+  const [state, setState] = useState({value:4})
   return (
     <View style={styles.container}>
       
       <Text style= {styles.textStyle}> 
         Open up App.js to start working on your app!
         </Text>
+        <View style={{ flex: 1, alignItems: 'stretch', justifyContent: 'center' }}>
+        <Slider
+          minimumValue={1}
+          maximumValue={7}
+          minimumTrackTintColor="#1EB1FC"
+          maximumTractTintColor="#1EB1FC"
+          step={1}
+          value={state.value}
+          onValueChange={value => setState({value})}
+          style={styles.slider}
+          thumbTintColor="#1EB1FC"
+        />
+  <Text>Value: {state.value}</Text>
+</View>
     </View>
   );
 }
@@ -19,6 +35,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     color: 'white',
     justifyContent: 'center',
+    
+  },
+  slider: {
+    position: 'absolute',
+    
+    width: 500,
+   
+    marginLeft: 125,
   },
   textStyle: {
     
